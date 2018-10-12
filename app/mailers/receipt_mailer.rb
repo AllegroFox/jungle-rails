6ee53bee07a75ea60285fbaa0d07ecbb7b487980
Emily McMinn <emily@mcminn.ca>
2018-10-12 18:01:27 +0000

    email receipt controllers

--- app/mailers/receipt_mailer.rb
+++ app/mailers/receipt_mailer.rb
@@ -1,2 +1,9 @@
 class ReceiptMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+  def receipt_email(user, order)
+    @user   = user
+    @order  = order
+    mail(to: @user.email, subject: 'Your receipt from Jungle: Order number #{order.id}')
+  end
 end
